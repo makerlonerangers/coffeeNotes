ad03768d9bde7e35fa68850efa0a9d154d4553e9
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-02-20 15:51:32 +0000

    add js backend doubles

---
+++ src/double.js
@@ -0,0 +1,12 @@
+const noteBook = {
+  getNotes(){},
+  editNote(){},
+  deleteNote(){},
+  createNote(){}
+}
+
+const note = {
+  title: "lone ranger",
+  body: "here is the body. It's an ok body, like 3 out of 10. not Jake though.",
+  update(){this.title = "a different title"}
+}
