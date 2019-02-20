a84e1995a13f3b8345d37e46c12dfed26f105037
William Dunk <williamdunk@gmail.com>
2019-02-20 17:22:31 +0000

    Add Note .body
    Co-authored-by: Ibrahim <ibrahimbutt1993@gmail.com>
    Co-authored-by: Jay <jpebs2016@gmail.com>
    Co-authored-by: Nikhil <nkhilv@gmail.com>
    Co-authored-by: Luca <lfruzza@hotmail.co.uk>

--- spec/note-spec.js
+++ spec/note-spec.js
@@ -3,4 +3,10 @@ describe('Note', () => {
     let note = new Note();
     expect(note.title != undefined).toEqual(true);
   });
+
+  it('can record the title and body of a note', () => {
+    let note = new Note('note1', 'body');
+    expect(note.title()).toEqual('note1');
+    expect(note.body()).toEqual('body');
+  });
 });
