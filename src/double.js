81a5d51bba5f463ffc8e5d70466ff5291f181b20
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-21 10:51:09 +0000

    modify displayNotes() function to render the list of notes on the page

--- src/double.js
+++ src/double.js
@@ -1,16 +1,18 @@
 const noteBook = {
-  all(){
-    return [note,note]
+  all() {
+    return [note, note];
   },
-  editNote(){},
-  deleteNote(){},
-  create(){
-    console.log("you made a note")
-  }
-}
+  editNote() {},
+  deleteNote() {},
+  create() {
+    console.log('you made a note');
+  },
+};
 
 const note = {
-  title: "lone ranger",
+  title: 'lone ranger',
   body: "here is the body. It's an ok body, like 3 out of 10. not Jake though.",
-  update(){this.title = "a different title"}
-}
+  update() {
+    this.title = 'a different title';
+  },
+};
