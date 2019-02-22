cf0d4b4a2285c4930ef1619b606d7f8af6f29e50
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-22 11:44:19 +0000

    Add interface.js
    Co-authored-by: Ibrahim <ibrahimbutt1993@gmail.com>
    Co-authored-by: Jay <jpebs2016@gmail.com>
    Co-authored-by: Luca <lfruzza@hotmail.co.uk>
    Co-authored-by: Will <williamdunk@gmail.com>

---
+++ src/interface.js
@@ -0,0 +1,73 @@
+console.log('Will');
+
+const noteBook = new NoteBook();
+
+const renderHome = () => {
+  document.querySelector('.container').innerHTML = `<div class="container">
+    <div class="container__form_container">
+      <form class="create_form">
+        <input
+          type="text"
+          name="note_title"
+          value=""
+          placeholder="Title"
+          required
+        />
+        <textarea
+          type="text"
+          name="note_body"
+          value=""
+          placeholder="body"
+        ></textarea>
+        <input type="submit" name="create_note" value="post!" />
+      </form>
+    </div>
+    <div class="display">
+      <ul class="display_notes"></ul>
+    </div>
+    <div class="edit"></div>
+  </div>`;
+};
+renderHome();
+
+const container = document.querySelector('.container');
+const form = document.querySelector('.create_form');
+const notesList = document.querySelector('.display_notes');
+
+const displayNotes = () => {
+  const notes = NoteBook.getAll();
+  notesList.innerHTML = notes
+    .map((note, index) => {
+      return `
+    <li class="single_note" id="note-${index}">
+      <h3>
+        ${note._title}
+      </h3>
+      <p>
+        ${note._body}
+      </p>
+      <button class="update-note">
+        Update
+      </button>
+    </li>
+    `;
+    })
+    .join('');
+};
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const title = form.querySelector('[name=note_title]').value;
+  const body = form.querySelector('[name=note_body]').value;
+  const noteObject = NoteBook.create(title, body);
+  displayNotes();
+  form.reset();
+});
+
+displayNotes();
+
+notesList.addEventListener('click', e => {
+  if (e.target.className !== 'update-note') return;
+
+  container.innerHTML = `<h1>This is the edit view</h1>`;
+});
